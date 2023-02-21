d559be43cb19b8654c96abf9b30c87cea2227809
Mitch Leahy <mitchleahy@Mitchs-MacBook-Pro.local>
2023-02-21 15:12:17 -0500

    fixed build errors

--- src/tuiter/home/feed-summary-item/icon-component.js
+++ src/tuiter/home/feed-summary-item/icon-component.js
@@ -9,20 +9,20 @@ const IconComponent = () => {
                 
             
             <div className = "col-3 ">
-                <a href = ""><FontAwesomeIcon icon={faHeart} /></a>
+                <a href = "/"><FontAwesomeIcon icon={faHeart} /></a>
                 <span className = "left-padding-12 font-lgrey"> 222</span>
             </div>
 
             <div className = "col-3">
-                <a href = ""><FontAwesomeIcon icon={faRetweet} /></a>
+                <a href = "/"><FontAwesomeIcon icon={faRetweet} /></a>
                 <span className = "left-padding-12 font-lgrey"> 1122</span>
             </div>
             <div className = "col-3">
-                <a href = ""><FontAwesomeIcon icon={faComment} /></a>
+                <a href = "/"><FontAwesomeIcon icon={faComment} /></a>
                 <span className = "left-padding-12 font-lgrey"> 292</span>
             </div>
             <div className = "col-3">
-                <a href = ""><FontAwesomeIcon icon={faShare} /></a>
+                <a href = "/"><FontAwesomeIcon icon={faShare} /></a>
                 <span className = "left-padding-12 font-lgrey"> 242</span>
             </div>
                  
